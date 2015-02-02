cca101a47a5535a08506af90fe890a3a2a3490fe
chutch01 <chutch01@hotmail.com>
2015-02-02 16:35:06 -0500

    fiddling with SuperHero class to get working

--- COMP1-11-Assignment2/src/SuperHero.java
+++ COMP1-11-Assignment2/src/SuperHero.java
@@ -7,18 +7,24 @@
  * due February 6th
  * date modified:  February 2
  */
-import java.util.Random;
-
 public class SuperHero extends Hero {
+//private variables
+	private int powerChoice;
+	
+	
+	
+	
+	
 //constructor
 	public SuperHero(String name) {
 		super(name);
-		System.out.println("picking powers");
 		generateRandomPowers();
 		
 	}
 //create String array in which we have 6 abilities, then generate a random number that chooses each at random
-	public void generateRandomPowers(){
+	private void generateRandomPowers(){
+		
+		//string array
 		String[]superPowers = new String[]{
 				"Super Speed",
 				"Super Strength",
@@ -28,9 +34,17 @@ public class SuperHero extends Hero {
 				"Weather Control"};
 	
 		
-		System.out.println("Picking powers...");
-		
+		//System.out.println("Picking powers...");
+		//generate a random number, pick that super power from the array and delete it, do it three times
+		for(int i=0; i >=2; i++){
+			this.powerChoice = (int)Math.random()*5;
+			System.out.println("Power to be deleted: " +powerChoice);
+			
+		}
+		//print the remaining super powers as the super powers that we want
+		System.out.println("Powers+++++++++++++");
 		for (String s: superPowers){
+			
 			System.out.printf("|%s|", s);
 			System.out.println();
 		}
